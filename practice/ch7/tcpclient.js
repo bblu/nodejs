001cffb9444341600e986194756a9b1d6a6cc632
bblu <gmwblu@gmail.com>
2017-01-27 16:26:19 +0800

    modify tcp client

--- practice/ch7/tcpclient.js
+++ practice/ch7/tcpclient.js
@@ -2,16 +2,14 @@ var assert = require('assert');
 var net = require('net');
 var clients = 0;
 var expectedAssertions = 2;
+var expectedId = 1;
+var client = net.connect(8000);
+client.on('data', function(data){
+    console.log(data.toString());
+    expectedAssertions--;
+    client.end();
+});
+client.on('end', function(){
+    console.log('over');
+});
 
-function runTest(expectedId, done){
-    var client = net.connect(8000);
-    client.on('data', function(data){
-        var expected = 'Welcome client: ' + expectedId + '\r\n';
-        assert.equal(data.toString(), expected);
-        expectedAssertions--;
-        client.end();
-    });
-
-    client.on('end', done);
-}
-runTest(expectedAssertions, function(){};
