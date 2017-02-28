e9325593491857dccd1e5b84d27c919b6746d946
wenbo.Lu <gmwblu@gmail.com>
2017-02-28 19:15:14 +0800

    Create app.js

---
+++ express/app.js
@@ -0,0 +1,62 @@
+// SET DEBUG=rout:* & npm start
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users); 
+
+app.route('/book')
+  .get(function(req, res) {
+	console.log('Get a random book2');
+    res.send('Get a random book');
+  })
+  .post(function(req, res) {
+	console.log('Add a book');
+    res.send('Add a book');
+  })
+  .put(function(req, res) {
+	console.log('Update the book');
+    res.send('Update the book');
+  });
+   
+   
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
