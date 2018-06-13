631e8bfa4366273e09a84292b1b20d21d6c4f5ae
bblu <gmwblu@gmail.com>
2018-06-13 13:57:00 +0800

    co

--- practice/generator.js
+++ practice/generator.js
@@ -30,3 +30,38 @@ g = genNum(0);
 console.log(g.next());
 console.log(g.next(2));
 console.log(g.next(3));
+
+//ch17.5.4 co
+//1.co函数接受Generator函数作为参数，返回Promise对象
+//2.然后将Generator函数的内部指针对象的next方法包装成onFulFilled函数捕捉错误。
+// fulfill your promise 履行你的诺言。
+function co(gen){
+    var ctx = this;
+    return new Promist(function(resolve, reject){
+        if(typeof gen === 'function'){
+            console.log('gen = gen.call(ctx)')
+            gen = gen.call(ctx);
+        }
+        if(!gen || typeof gen.next !== 'function'){
+            console.log('return resolve(gen)');
+            return resolve(gen);
+        };
+        onFulfilled();
+        function onFulfilled(res){
+            var ret;
+            try{
+                ret = gen.next(res);
+            }catch(e){
+                return reject(e);
+            }
+            next(ret);
+        }
+    });
+}
+//关键的next函数反复调用自身
+function next(ret){
+    if(ret.done) return resolve(ret.value);
+    var value = toPromise.call(ctx,ret.value);
+}
+
+//ch17.5.5.并发异步操作 co
