7a876f92a26c8f7b7ed52fbe01be8a9d65b68b86
bblu <gmwblu@gmail.com>
2018-06-16 12:24:50 +0800

    class

--- practice/newclass.js
+++ practice/newclass.js
@@ -1,3 +1,59 @@
+//ES6::ch19::Class 为了接近OOP语言的语法而引入的关键字 是function
+
+function Point(x,y){
+    this.x = x;
+    this.y = y;
+    function privateFoo(p){
+        console.log('do somthing foo inside for '+ p);
+    }
+    function privateBar(){
+        console.log('do somthing bar inside for '+ p);        
+    }
+    this.publicInterface = function(param){
+        privateFoo(param);
+        privateBar(param);
+    }
+}
+Point.prototype.toString = function(){
+    let str = `Point(${this.x},${this.y})`;
+    return str;
+};
+//=用ES Class改写=>
+class NewPoint{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    //类方法既不需要function也不需要逗号分割
+    //此处的格式可以参考原来function形式内部函数的写法格式上是一致的。
+    toString(){
+        return `NewPoint(${this.x},${this.y})`;
+    }
+    move(deltx,delty){
+        let bp = this.toString();
+        this.x += deltx;
+        this.y += delty;
+        console.log(`move ${bp} to ${this.toString()}`);
+    }
+}
+let p = new Point(0,0);
+console.log(p.toString());
+p.publicInterface('whatyouwant');
+let n = new NewPoint(1,1);
+//(type of NewPoint is "function") => 类的数据类型是函数！
+console.log(`type of NewPoint = ${typeof NewPoint}`);
+console.log(`type of constructor = ${typeof NewPoint.prototype.constructor}`);
+if(NewPoint === NewPoint.prototype.constructor){
+    //NewPoint类本身指向构造函数，实际上类的所有方法都定义在prototype上
+    console.log('NewPoint === NewPoint.prototype.constructor')
+}else{
+    console.log('NewPoint !== NewPoint.prototype.constructor')    
+}
+
+console.log(n.toString());
+n.move(1,-1);
+
+
 class MyClass{
     constructor(...args){
         this._args = args;
@@ -7,11 +63,11 @@ class MyClass{
         console.log(`constructor for instance counter = ${MyClass._counter}`);
     }
     //静态方法需要从类调用，不能由实例发起；
-
     static getInstanceCount(){
         console.log(`MyClass Instance Count = ${this._counter}`);
         return MyClass._counter;
     }
+    //私有方法
 
     * [Symbol.iterator](){
         for(let arg of this._args){
